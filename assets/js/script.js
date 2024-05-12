993123142f1a8a5cd3ecfcf27a0d5c517633c684
Eesa Wazir <eesawazir9@gmail.com>
2024-05-12 21:05:33 +0500

    Formatting files using Prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,43 @@
-const menuButton = document.getElementById('menu-open');
-const navigation = document.querySelector('nav');
-const menuExitButton = document.getElementById('menu-exit');
+const menuButton = document.getElementById("menu-open");
+const navigation = document.querySelector("nav");
+const menuExitButton = document.getElementById("menu-exit");
 
-menuButton.addEventListener('click', () => {
-    navigation.classList.add('menu-button')
-})
+menuButton.addEventListener("click", () => {
+    navigation.classList.add("menu-button");
+});
 
-menuExitButton.addEventListener('click', () => {
-    navigation.classList.remove('menu-button')
-})
+menuExitButton.addEventListener("click", () => {
+    navigation.classList.remove("menu-button");
+});
 
 function smoothScroll(target) {
     const element = document.querySelector(target);
     if (element) {
-        const headerHeight = document.querySelector('header').offsetHeight;
+        const headerHeight = document.querySelector("header").offsetHeight;
         const offset = element.getBoundingClientRect().top - headerHeight;
-        window.scrollBy({ top: offset, left: 0, behavior: 'smooth' });
+        window.scrollBy({ top: offset, left: 0, behavior: "smooth" });
     }
 }
 
-const homeLink = document.getElementById('hero-link');
-const aboutLink = document.getElementById('about-link');
-const projectsLink = document.getElementById('projects-link');
+const homeLink = document.getElementById("hero-link");
+const aboutLink = document.getElementById("about-link");
+const projectsLink = document.getElementById("projects-link");
 
 if (homeLink) {
-    homeLink.addEventListener('click', (e) => {
+    homeLink.addEventListener("click", (e) => {
         e.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+        window.scrollTo({ top: 0, behavior: "smooth" });
     });
 }
 if (aboutLink) {
-    aboutLink.addEventListener('click', (e) => {
+    aboutLink.addEventListener("click", (e) => {
         e.preventDefault();
-        smoothScroll('#about');
+        smoothScroll("#about");
     });
 }
 if (projectsLink) {
-    projectsLink.addEventListener('click', (e) => {
+    projectsLink.addEventListener("click", (e) => {
         e.preventDefault();
-        smoothScroll('#projects');
+        smoothScroll("#projects");
     });
 }
